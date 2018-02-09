6162b1c4af526b89723bf32fb81d35a52e394046
Dan Birt <danbirt@gmail.com>
2018-02-08 21:02:36 -0600

    Mark selected note in list

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,16 @@
+export const notes = [
+    {
+        _id: 'noteId1',
+        title: 'Test title',
+        body: '',
+        updatedAt: 1518132605622,
+        userId: 'userId1'
+    },
+    {
+        _id: 'noteId2',
+        title: '',
+        body: 'Something is here.',
+        updatedAt: 1518132605622,
+        userId: 'userId2'
+    }
+];
